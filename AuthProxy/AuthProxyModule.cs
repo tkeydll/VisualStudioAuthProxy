6bb7fdb61a17323129bc3831475e6962713271bc
tkey.dll <tkey.dll@gmail.com>
2016-04-23 22:19:36 +0900

    First commit.

---
+++ AuthProxy/AuthProxyModule.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+
+namespace AuthProxy
+{
+    public class AuthProxyModule : IWebProxy
+    {
+        ICredentials crendential = new NetworkCredential("USER", "PASS");
+
+        public ICredentials Credentials
+        {
+            get
+            {
+                return crendential;
+            }
+            set
+            {
+                crendential = value;
+            }
+        }
+
+        public Uri GetProxy(Uri destination)
+        {
+            return new Uri("http://YOUR_HOST:PORT", UriKind.Absolute);
+        }
+
+        public bool IsBypassed(Uri host)
+        {
+            return host.IsLoopback;
+        }
+
+    }
+}
